9aa5d93a284b44f00c8e2190276f93879860d9f1
KevinP26 <kperezobando16@gmail.com>
2025-05-06 23:02:38 -0600

    Creacion de proyecto y conexion a base de datos

---
+++ src/test/java/sv/edu/uca/Laboratorio02/Laboratorio02ApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.uca.Laboratorio02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
